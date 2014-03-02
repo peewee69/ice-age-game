278ef2aaab2719a45422576617d267c6c36c9b48
Jonathan Hoult <jonhoult@hotmail.com>
2014-03-02 15:33:22 +1100

    Incorporated sliding page framework, removed redundant frameworks.

---
+++ Content/scripts/ice-age-main.js
@@ -0,0 +1,16 @@
+﻿window.IceAge = {
+    CalculateEndResult: function() {
+        var data = window.IceAge.DataPoints,
+            vals = window.IceAge.Values;
+
+        return data[vals.precession][vals.eccentricity][vals.obliquity];
+    },
+    Values: {
+        precession: undefined,
+        eccentricity: undefined,
+        obliquity: undefined
+    },
+    runEndSequence: function ($endSlide, params) {
+        alert(window.IceAge.CalculateEndResult());
+    }
+}
