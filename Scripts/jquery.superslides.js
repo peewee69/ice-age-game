c6fb1e157ecdd7cb205e50c446fa121d45dce495
Jonathan Hoult <jonhoult@hotmail.com>
2014-03-04 02:36:18 +1100

    Removed ability to slide to new panels using keyboard.

--- Scripts/jquery.superslides.js
+++ Scripts/jquery.superslides.js
@@ -50,15 +50,6 @@ Superslides = function(el, options) {
       }
     });
 
-    $(document).on('keyup', function(e) {
-      if (e.keyCode === 37) {
-        that.animate('prev');
-      }
-      if (e.keyCode === 39) {
-        that.animate('next');
-      }
-    });
-
     $(window).on('resize', function() {
       setTimeout(function() {
         var $children = that.$container.children();
